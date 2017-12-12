8258f35180587d18ad91a0a487fc3f8d268e886c
Paul Lucas <paullucas87@gmail.com>
2017-12-12 21:30:35 +0000

    Add Additional Race paces
    Added race paces to the VDOT calculations and file generation.

--- VDOT/VDOT Training File.py
+++ VDOT/VDOT Training File.py
@@ -1,35 +1,66 @@
 from collections import OrderedDict
-from settings.converters import convert_to_time, calculate_pace, time_to_string
+from settings.converters import convert_to_time, calculate_pace, time_to_string, convert_distance, dec
 
-def get_vdot_race_paces_EMT():
+
+distances = {
+    '1,500': [dec('1.5', 1), convert_distance(dec('1.5', 1), 'km', 'mile')],
+    'Mile': [convert_distance(dec('1', 0), 'mile', 'km'), dec('1', 0)],
+    '3,000': [dec('3', 0), convert_distance(dec('3', 0), 'km', 'mile')],
+    '2mile': [convert_distance(dec('2', 0), 'mile', 'km'), dec('2', 0)],
+    '5,000': [dec('5', 0), convert_distance(dec('5', 0), 'km', 'mile')],
+    '10K': [dec('10', 0), convert_distance(dec('10', 0), 'km', 'mile')],
+    '15K': [dec('15', 0), convert_distance(dec('10', 0), 'km', 'mile')],
+    'HalfMarathon': [convert_distance(dec('13.11', 2), 'mile', 'km'), dec('13.11', 2)],
+    'Marathon': [convert_distance(dec('26.22', 2), 'mile', 'km'), dec('26.22', 2)]
+}
+
+def get_vdot_race_paces(file):
     """Extracts the race paces from the VDOT Races file."""
-    VDOT = OrderedDict()
+    vdot = OrderedDict()
     headers = []
-    with open('VDOT EMT.txt', 'r') as VDOT_file:
+    with open(file, 'r') as VDOT_file:
         for line in VDOT_file.readlines():
-            cells = line.split() # Ignore last column as this is repeated.
+            cells = line.split()  # Ignore last column as this is repeated.
             if cells[0] == 'VDOT':
                 headers += cells[1:]
             else:
                 cell_times = [x for x in cells[1:]]
-                VDOT[int(cells[0])] = dict(zip(headers, cell_times))
+                vdot[int(cells[0])] = dict(zip(headers, cell_times))
 
-    return VDOT
+    return vdot
 
-def get_vdot_race_paces_IR():
+
+def fix_ir_file():
     """Extracts the race paces from the VDOT Races file."""
-    VDOT = OrderedDict()
-    headers = []
+    new_lines = ''
     with open('VDOT IR.txt', 'r') as VDOT_file:
         for line in VDOT_file.readlines():
             cells = line.split()  # Ignore last column as this is repeated.
-            if cells[-1] == 'VDOT':
-                headers += cells[:-1]
-            else:
-                cell_times = [x for x in cells[:-1]]
-                VDOT[int(cells[-1])] = dict(zip(headers, cell_times))
+            new_lines += ' '.join([cells[-1]] + cells[:-1]) + '\n'
+            with open('VDOT IR Fixed.txt', 'w') as new_VDOT_file:
+                new_VDOT_file.write(new_lines)
+        print('Formatted.')
 
-    return VDOT
+
+def get_race_paces(races):
+    paces={}
+    recovery_km = convert_to_time('01:15')
+    recovery_mile = convert_to_time('02:00')
+    for vdot, race_times in races.items():
+        paces[vdot] = {}
+        for race, times in race_times.items():
+            if vdot == 30:
+                print(race, convert_to_time(times), distances[race][0], distances[race][1])
+            paces[vdot][race + '-KM'] = time_hhmm(calculate_pace(convert_to_time(times), distances[race][0], 'km'))
+            paces[vdot][race + '-Mile'] = time_hhmm(calculate_pace(convert_to_time(times), distances[race][1], 'mile'))
+            if race == 'HalfMarathon':
+                paces[vdot]['Recovery-KM'] = time_hhmm(calculate_pace(convert_to_time(times)
+                                                                     , distances[race][0], 'km') +
+                                                      recovery_km)
+                paces[vdot]['Recovery-Mile'] = time_hhmm(calculate_pace(convert_to_time(times)
+                                                                       , distances[race][1], 'mile') +
+                                                        recovery_mile)
+    return paces
 
 
 def time_hhmm(time):
@@ -66,21 +97,31 @@ def get_repetition_time(repetition, pace):
         return time_hhmm(calculate_pace(convert_to_time(repetition['Repetition200']), 200, 'metre', pace))
 
 
+
+
 if __name__ == '__main__':
-    vdots = get_vdot_race_paces_EMT()
-    vdots_ir = get_vdot_race_paces_IR()
+    fix_ir_file()
+
+    vdots = get_vdot_race_paces('VDOT EMT.txt')
+    vdots_ir = get_vdot_race_paces('VDOT IR Fixed.txt')
+
+    vdot_races = get_vdot_race_paces('VDOT Races.txt')
+    race_paces = get_race_paces(vdot_races)
 
     vdot_lines = [
-        'VDOT Easy-KM Easy-Mile Long-KM Long-Mile Marathon-KM Marathon-Mile Threshold-400 Threshold-KM Threshold-Mile' +
+        'VDOT Easy-KM Easy-Mile Long-KM Long-Mile Marathon-KM Marathon-Mile Threshold-KM Threshold-Mile' +
+        ' Recovery-KM Recovery-Mile' +
         # ' Interval400 IntervalKM Interval1200 IntervalM' +
         # ' Repetition200 Repetition300 Repetition400 Repetition600 Repetition800' +
-        ' Interval-KM Interval-Mile Repetition-KM Repetition-Mile\n'
+        ' Interval-KM Interval-Mile Repetition-KM Repetition-Mile' +
+        ' 1,500-KM 1,500-Mile Mile-KM Mile-Mile 3,000-KM 3,000-Mile 2Mile-KM 2Mile-Mile 5,000-KM 5,000-Mile' +
+        ' 10K-KM 10K-Mile 15K-KM 15K-Mile HalfMarathon-KM HalfMarathon-Mile Marathon-KM Marathon-Mile\n'
     ]
 
-    line = ' '.join(['{}'] * len(vdot_lines[0].split())) + '\n'
+    line_fmt = ' '.join(['{}'] * len(vdot_lines[0].split())) + '\n'
 
     for k, v in vdots.items():
-        vdot_lines.append(line.format(
+        vdot_lines.append(line_fmt.format(
             k,
             v['Easy/LongKM'].split('-')[0],
             v['Easy/LongM'].split('-')[0],
@@ -88,22 +129,42 @@ if __name__ == '__main__':
             v['Easy/LongM'].split('-')[1],
             v['MarathonKM'],
             v['MarathonM'],
-            v['Threshold400'],
+            # v['Threshold400'],
             v['ThresholdKM'],
             v['ThresholdM'],
-            #vdots_ir[k]['Interval400'],
-            #vdots_ir[k]['IntervalKM'],
-            #vdots_ir[k]['Interval1200'],
-            #vdots_ir[k]['IntervalM'],
-            #vdots_ir[k]['Repetition200'],
-            #vdots_ir[k]['Repetition300'],
-            #vdots_ir[k]['Repetition400'],
-            #vdots_ir[k]['Repetition600'],
-            #vdots_ir[k]['Repetition800'],
-            get_interval_time(vdots_ir[k], 'mile'),
+            race_paces[k]['Recovery-KM'],
+            race_paces[k]['Recovery-Mile'],
+            # vdots_ir[k]['Interval400'],
+            # vdots_ir[k]['IntervalKM'],
+            # vdots_ir[k]['Interval1200'],
+            # vdots_ir[k]['IntervalM'],
+            # vdots_ir[k]['Repetition200'],
+            # vdots_ir[k]['Repetition300'],
+            # vdots_ir[k]['Repetition400'],
+            # vdots_ir[k]['Repetition600'],
+            # vdots_ir[k]['Repetition800'],
             get_interval_time(vdots_ir[k], 'km'),
+            get_interval_time(vdots_ir[k], 'mile'),
+            get_repetition_time(vdots_ir[k], 'km'),
             get_repetition_time(vdots_ir[k], 'mile'),
-            get_repetition_time(vdots_ir[k], 'km')
+            race_paces[k]['1,500-KM'],
+            race_paces[k]['1,500-Mile'],
+            race_paces[k]['Mile-KM'],
+            race_paces[k]['Mile-Mile'],
+            race_paces[k]['2mile-KM'],
+            race_paces[k]['2mile-Mile'],
+            race_paces[k]['3,000-KM'],
+            race_paces[k]['3,000-Mile'],
+            race_paces[k]['5,000-KM'],
+            race_paces[k]['5,000-Mile'],
+            race_paces[k]['10K-KM'],
+            race_paces[k]['10K-Mile'],
+            race_paces[k]['15K-KM'],
+            race_paces[k]['15K-Mile'],
+            race_paces[k]['HalfMarathon-KM'],
+            race_paces[k]['HalfMarathon-Mile'],
+            race_paces[k]['Marathon-KM'],
+            race_paces[k]['Marathon-Mile'],
         ))
 
     with open('VDOT Training.txt', 'w') as training_file:
--- VDOT/VDOT.py
+++ VDOT/VDOT.py
@@ -1,11 +1,20 @@
 from collections import OrderedDict
-from settings.converters import convert_to_time, dec
-from settings.settings import VDOT_RACES, VDOT_TRAINING
+import datetime
+from settings.converters import convert_to_time, dec, time_to_string
+from settings.settings import Settings
+import json
+import os
+
+settings = Settings()
+
 
 class VDOT:
     """Contains the VDOT values for the race and training paces."""
     def __init__(self):
         self.VDOT_paces = set_vdots()
+        self.vdot_score = dec('0.00', 2)
+        self.training_paces = {}
+        self.race_times = {}
 
     def __getitem__(self, item):
         return self.VDOT_paces[item]
@@ -19,7 +28,8 @@ class VDOT:
         full_vdot = self.current_vdot(distance, race_time)
         vdot_distance_diff = self._vdot_difference(full_vdot, distance)
         finish_distance_diff = self._vdot_finish_diff(full_vdot, distance, race_time)
-        return full_vdot + dec(finish_distance_diff / vdot_distance_diff, 2, 'ROUND_FLOOR')
+        self.vdot_score = full_vdot + dec(finish_distance_diff / vdot_distance_diff, 2, 'ROUND_FLOOR')
+        self._get_training_paces()
 
     def current_vdot(self, distance, time):
         """Gets the VDOT for the given time."""
@@ -33,7 +43,6 @@ class VDOT:
         """Determines the time difference between a given VDOT score and the next level up for a given distance."""
         current_vdot = self.VDOT_paces[score]
         next_vdot = self.VDOT_paces[score+1]
-
         return current_vdot[distance] - next_vdot[distance]
 
     def _vdot_finish_diff(self, score, distance, time):
@@ -41,13 +50,52 @@ class VDOT:
         vdot_time = self.VDOT_paces[score][distance]
         return vdot_time - time
 
+    def _get_training_paces(self):
+        """Returns the training paces for the current vdot score."""
+        if self.vdot_score == dec('0.00', 2):
+            return
+        vdot_diff = self._vdot_dec()
+        self._calculate_training_paces(vdot_diff)
+        return None
+
+    def _calculate_training_paces(self, vdot_diff):
+        """Calculates the difference between the vdot score and the given race time."""
+        vdot_score = int(self.vdot_score)
+        for distance, current_pace in self.VDOT_paces[vdot_score].items(): #[(k, v) for k, v in self.VDOT_paces[vdot_score].items()
+                                       #if k.endswith('-KM') or k.endswith('-Mile')]:
+            pace_diff = self._vdot_difference(vdot_score, distance)
+            pace_to_add = datetime.timedelta(seconds=int(dec(dec(pace_diff.total_seconds()) * vdot_diff, 0)))
+            if distance.endswith('-KM') or distance.endswith('-Mile'):
+                self.training_paces[distance] = current_pace - pace_to_add
+            else:
+                self.race_times[distance] = current_pace - pace_to_add
+
+    def _vdot_dec(self):
+        """Returns the decimal of the current vdot score."""
+        return self.vdot_score - dec(self.vdot_score, 0, 'ROUND_FLOOR')
+
+    def save_vdot(self):
+        """Saves the paces and vdot to the config for later use."""
+        settings.amend_config('paces', self.format_training_paces())
+        settings.amend_config('vdot', str(self.vdot_score))
+        settings.amend_config('race_times', self.format_race_paces())
+
+    def format_training_paces(self):
+        """Formats the times of the paces so they can be saved to the config."""
+        fmt = '{minutes}:{seconds:02}'
+        return {k: time_to_string(v, fmt) for k, v in self.training_paces.items()}
+
+    def format_race_paces(self):
+        """Formats the times of the paces so they can be saved to the config."""
+        fmt = '{hours}:{minutes}:{seconds:02}'
+        return {k: time_to_string(v, fmt) for k, v in self.race_times.items()}
+
 
 def set_vdots():
     """Creates a dictionary from the combined extracts of vdot race and training paces."""
     vdots = OrderedDict()
-    vdots = get_vdot_race_paces(vdots, VDOT_RACES)
-    vdots = get_vdot_race_paces(vdots, VDOT_TRAINING)
-
+    vdots = get_vdot_race_paces(vdots, settings.VDOT_RACES)
+    vdots = get_vdot_race_paces(vdots, settings.VDOT_TRAINING)
     return vdots
 
 
@@ -70,10 +118,33 @@ def get_vdot_race_paces(vdots, file_name):
 if __name__ == '__main__':
 
     VDOT_values = VDOT()
-    #print(str(VDOT_values))
+    # print(str(VDOT_values))
 
-    print(VDOT_values[46])
+    # print(VDOT_values[51])
+    print('\n51 Times')
+    for k1, v1 in [(k1, v1) for k1, v1 in VDOT_values[51].items()
+                   if k1.endswith('-KM') or k1.endswith('-Mile')]:
+        print('{} - {}'.format(k1, v1))
 
-    #print(VDOT_values.current_vdot('Marathon', '3:15:45'))
-    print(VDOT_values.calculate_vdot('HalfMarathon', '1:29:38'))
+    print('\n52 Times')
+    for k1, v1 in [(k1, v1) for k1, v1 in VDOT_values[52].items()
+                   if k1.endswith('-KM') or k1.endswith('-Mile')]:
+        print('{} - {}'.format(k1, v1))
 
+    # print(VDOT_values.current_vdot('Marathon', '3:15:45'))
+    VDOT_values.calculate_vdot('HalfMarathon', '1:41:00')
+
+    print('\nTraining Paces')
+    for k1, v1 in VDOT_values.training_paces.items():
+        if k1.endswith('-Mile'):
+            print('{} - {}'.format(k1, v1))
+
+    print('\nRace Times')
+    for k1, v1 in VDOT_values.race_times.items():
+        print('{} - {}'.format(k1, v1))
+
+    print(VDOT_values.vdot_score)
+
+    print(VDOT_values.vdot_score - dec(VDOT_values.vdot_score, 0, 'ROUND_FLOOR'))
+
+    VDOT_values.save_vdot()
