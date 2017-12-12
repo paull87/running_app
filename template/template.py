870810d6c066ffeba726a221b6c64e8f751d49f6
Paul Lucas <paullucas87@gmail.com>
2017-12-12 21:38:18 +0000

    Additional Templates
    Amended the schedule and workout templates.
    Also added additional json files for some of the default schedule workout plans.

--- template/template.py
+++ template/template.py
@@ -1,17 +1,28 @@
 from settings.converters import timestamp
-from settings.settings import TEMPLATE_PATH
+from settings.settings import Settings
+
+settings = Settings()
+
+
+def get_schedule_template():
+    """Gets the schedule template needed to create a schedule file."""
+    try:
+        with open(settings.SCHEDULE_PATH, 'r') as template:
+            return template.read()
+    except Exception:
+        raise FileNotFoundError('Unable to open the schedule file.') from None
 
 
 def get_workout_template():
-    """Gets the workout """
+    """Gets the workout template needed to create a workout file."""
     try:
-        with open(TEMPLATE_PATH, 'r') as template:
+        with open(settings.TEMPLATE_PATH, 'r') as template:
             return template.read()
-    except Exception as e:
+    except Exception:
         raise FileNotFoundError('Unable to open the template file.') from None
 
 
 if __name__ == '__main__':
     workout_template = get_workout_template()
 
-    print(workout_template.format(timestamp(), 'This is a Test Run.'))
+    print(workout_template.format(timestamp(), 'This is a Test Run.', 9999))
